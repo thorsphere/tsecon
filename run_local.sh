6626e7b9c53f82cd9ce64d738e77804e6b679cf2
thorsphere <github@thorsphere.com>
2026-05-30 14:05:26 +0000

    add GitHub Actions deployment workflow

--- run_local.sh
+++ run_local.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# Copyright (c) 2026 thorsphere.
+# All Rights Reserved. Use is governed with GNU Affero General Public License v3.0
+# that can be found in the LICENSE file.
+
 set -euo pipefail
 
 # ------------------------------------------------------------------
